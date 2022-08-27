6d8e6e22ceb4bc7aa0087f54a58662dff48adf5c
Eipek95 <eipek1995@gmail.com>
2022-08-27 21:37:21 +0300

    Add Claims && add webapi appsettings token property

---
+++ Core3/Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core3.Entities.Concrete
+{
+    public class User:IEntity
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public bool Status { get; set; }
+    }
+}
